e1f3e763e28cbeabc7ec4e58652c70e0fd07cf5e
Chris <retpada@gmail.com>
2018-12-01 20:41:55 -0800

    Update weather.ps1

--- weather.ps1
+++ weather.ps1
@@ -1,2 +1,12 @@
-$weather = Invoke-WebRequest -Uri "https://w1.weather.gov/xml/current_obs/KPWT.xml" -UseBasicParsing
-$weather 
+function get-weather {
+    param ( 
+        # This will pick between Farenheit or Celcius 
+        [Parameter(Mandatory = $True )]
+        [ValidateSet("C","F")]
+        [string]
+        $TempScale )
+    $weather = Invoke-WebRequest -Uri "https://w1.weather.gov/xml/current_obs/KPWT.xml" -UseBasicParsing
+    $temp = [xml] $weather.Content
+    if ($TempScale -eq "F") {$temp.current_observation.temp_f}
+    else {$temp.current_observation.temp_c}
+}
